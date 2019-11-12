389f965b3899195bccf8d378eb8a5e0fced6f05e
Alexis Chilinski <chilinski.a@gmail.com>
2019-11-12 01:44:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,9 +36,9 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if (1...9).to_a.include?(input.to_i) != nil 
+  if (1...9).to_a.include?(input.to_i)
     puts "Playing #{songs[input.to_i - 1]}"
-    elsif songs.include?(input) != nil 
+    elsif songs.include?(input)
     puts "Playing #{input}"
   else
     puts "Invalid input, please try again"
